f70c57a094de404cef9ffba1ece60e2d0a95ff41
RTH <79984800+ri-tae-ka@users.noreply.github.com>
2021-05-16 11:16:58 +0530

    Add initial files

---
+++ calculator.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    var num1 = Number(req.body.n1);
+    var num2 = Number(req.body.n2);
+
+    var result = num1 + num2;
+
+    res.send("Result of the calculation is " + result);
+});
+
+app.listen(1303, function() {
+    console.log("Server is running on port 1303");
+});
